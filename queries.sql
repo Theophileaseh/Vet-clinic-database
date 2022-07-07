a2e8a18b6c380445cb27a19e0803e50d48cf8a91
Theophileaseh <theophilenwachan@gmail.com>
2022-07-07 12:42:38 +0100

    Update queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,36 @@ SELECT (name, escape_attempts) from animals WHERE weight_kg > 10.5
 SELECT * FROM animals WHERE neutered = True
 SELECT * FROM animals WHERE name = 'Gabumon'
 SELECT * FROM animals WHERE (weight_kg between 10.4 AND 17.3) OR (weight_kg = 10.4 OR weight_kg = 17.3)
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT * from animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+
+SELECT MAX(weight_kg), MIN(weight_kg), species from animals WHERE species = 'pokemon' OR species = 'digimon' GROUP BY species;
+
+SELECT AVG(escape_attempts), species FROM animals WHERE (SELECT EXTRACT(YEAR FROM date_of_birth) AS year) BETWEEN '1990' AND '2000' GROUP BY species;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+
+UPDATE animals SET species = '';
+COMMIT;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species = '';
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+COMMIT;
+
+BEGIN;
+DELETE * from animals WHERE date_of_birth > '01-01-2022';
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
